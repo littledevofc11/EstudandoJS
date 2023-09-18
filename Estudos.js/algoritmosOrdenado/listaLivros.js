e78367ca1339aeeab5328641c4c7935194c55555
Enzo <enzoggw015@gmail.com>
2023-09-18 20:28:26 -0300

    Inicio sobre algoritmos

---
+++ Estudos.js/algoritmosOrdenado/listaLivros.js
@@ -0,0 +1,29 @@
+const precoLivros = [
+    {
+        titulo:"Javascript",
+        preco:25,
+    },
+    {
+        titulo:"PHP",
+        preco:15,
+    },
+    {
+        titulo:"Java",
+        preco:30,
+    },
+    {
+        titulo:"Elixir",
+        preco:50,
+    },
+    {
+        titulo:"Go",
+        preco:45,
+    },
+    {
+        titulo:"Python",
+        preco:20,
+    }
+]
+
+//modulo para exportar para o codigo principal
+module.exports = precoLivros;
---
+++ Estudos.js/algoritmosOrdenado/selectionSort.js
@@ -0,0 +1,6 @@
+const livros = require('./listaLivros')
+const menorValor = require('./menorValor')
+
+for(let atual = 0; atual < livros.length; atual++){
+    
+}
