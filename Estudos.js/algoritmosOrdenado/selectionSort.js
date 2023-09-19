8c2e0acca79892e80f4e267ea968b68c911b2b46
Enzo <enzoggw015@gmail.com>
2023-09-19 17:46:20 -0300

    algoritmos e analises

--- Estudos.js/algoritmosOrdenado/selectionSort.js
+++ Estudos.js/algoritmosOrdenado/selectionSort.js
@@ -1,6 +1,22 @@
 const livros = require('./listaLivros')
 const menorValor = require('./menorValor')
 
-for(let atual = 0; atual < livros.length; atual++){
+for(let atual = 0; atual < livros.length - 1; atual++){
+    let menor = menorValor(livros, atual)
+
+    let livroAtual = livros[atual]
     
+    let livroMenorPreco = livros[menor]
+    
+
+    livros[atual] = livroAtual
+    
+    livros[menor] = livroMenorPreco
+
+    console.log("posicao", atual)
+    console.log("O livro com menor preco é", livros[menor])
+    console.log("O livro com atual é ", livros[atual])
+
 }
+
+console.log(livros)
